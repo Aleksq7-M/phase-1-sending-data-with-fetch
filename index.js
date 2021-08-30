ce2841480cd90ec5c8a3f7773115346945bb538e
aleksq7 <morrisalex2001@gmail.com>
2021-08-29 23:45:54 -0600

    commit tree 2d7ac45284de499a81f5868cfb1085d8a9a73881

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your code here
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+        dogName: 'Byron',
+        dogBreed: 'Poodle'
+    })
+};
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(resp => resp.json())
+.then(obj => console.log(obj))
+.catch(function(error){
+    alert('bad Things! ragnarok!');
+    console.log(error.message);
+});
+
+function submitData(userName, userEmail){
+    const userObj = {
+        name: userName,
+        email: userEmail
+    }
+
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userObj)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(resp => resp.json())
+    .then(function(obj){
+        handleResp(obj)
+    })
+    .catch(error => handleError(error));
+}
+
+function handleResp(object){
+    let h1 = document.createElement('h1');
+    let body = document.querySelector('body');
+    h1.textContent = (object.id);
+    body.appendChild(h1);
+}
+
+function handleError(err){
+    let h1 = document.createElement('h1');
+    let body = document.querySelector('body');
+    h1.textContent = err.message;
+    body.appendChild(h1);
+    // console.log(err);
+}
